40d82f34de40f39031d3d723df977b336e89cfea
keki94 <quequisegarra@gmail.com>
2021-08-14 11:44:33 +0100

    probably the only way to actually import the files

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {SocialNav, ThemeChanger} from '../../components'
 import './style.css';
+import Logo from '../../assets/Dayflow.png'
 
 export default () => { 
     return (
@@ -8,7 +9,7 @@ export default () => {
             <ThemeChanger className='theme'/>
             <div className="home">
             <header>
-                <div className="logo" ><img src="../../assets/Dayflow.png" alt="me"/></div>
+                <div className="logo" ><img src={Logo} alt="me"/></div>
                 <h1>Mónica <br></br> &emsp; Segarra</h1>
             </header>
             </div>
